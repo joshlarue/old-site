c856b16ba6a7fb8c260fb00204cfe1ce4b5a3ae9
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-09-10 22:00:13 -0600

    full button functionality and player input logic finished

--- testing.js
+++ testing.js
@@ -1,6 +1,5 @@
 let score = 0;
 let cScore = 0;
-let round = 0;
 let playerChoice;
 
 const playerScore = document.querySelector(".player-score");
@@ -9,22 +8,27 @@ const rockBtn = document.querySelector(".rock");
 const paperBtn = document.querySelector(".paper");
 const scissorsBtn = document.querySelector(".scissors");
 const resetBtn = document.querySelector(".reset");
+const result = document.querySelector(".result");
 
 function playerInput() {
     rockBtn.addEventListener("click", () => {
-        playerChoice = "rock"; 
+        playerChoice = "rock";
+        playRound(playerChoice, getComputerChoice());
     });
 
     paperBtn.addEventListener("click", () => {
         playerChoice = "paper";
+        playRound(playerChoice, getComputerChoice());
     });
 
     scissorsBtn.addEventListener("click", () => {
         playerChoice = "scissors";
+        playRound(playerChoice, getComputerChoice());
     });
 
     resetBtn.addEventListener("click", () => {
-        game();
+        score = 0;
+        cScore = 0;
     });
 }
 
@@ -60,19 +64,7 @@ function playRound(playerChoice, computerChoice) {
             cScore++;
             computerScore.textContent = `Computer score is ${cScore}.`;
         }
-    round++;
-    console.log(round);
     return score;
 }
 
-function game() {
-    console.log("game started");
-    score = 0;
-    cScore = 0;
-    playerInput();
-    if (round > 3) {
-        console.log("Game over!");
-    }
-}
-
-game();
+playerInput();
