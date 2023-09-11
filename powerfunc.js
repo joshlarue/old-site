c21760c1c9b87408bc79b2b64e54474f8711df1a
joshlarue <4442031+joshlarue@users.noreply.github.com>
2023-09-10 21:28:28 -0600

    removed old files

--- powerfunc.js
+++
@@ -1,7 +0,0 @@
-function pow(x, n) {
-    let result = 1;
-    for (let i = 0; i < n; i++) {
-      result *= x;
-    }
-    return result;
-}
--- youpressed.js
+++
@@ -1,6 +0,0 @@
-const textbox = document.querySelector("#textbox");
-const output = document.querySelector("#output");
-
-textbox.addEventListener("keydown", (event) => {
-    output.textContent = `you pressed "${event.key}"`;
-});
