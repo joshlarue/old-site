a23a4f24d5bcfa57bf804fece3dc7614a0d2056e
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-10-05 17:30:38 -0600

    initial setup, added vars + queryselectors

---
+++ math.js
@@ -0,0 +1,4 @@
+let prompt = document.querySelector(".prompt");
+let input = document.querySelector(".input-field")
+let submit = document.querySelector(".submit-button")
+let result = document.querySelector(".result")
