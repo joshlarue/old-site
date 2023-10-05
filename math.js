894e87678e0f753d37d50156c95c9b4b8c0afe0d
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-10-05 17:39:51 -0600

    first run of code, not yet tested

--- math.js
+++ math.js
@@ -1,4 +1,20 @@
 let prompt = document.querySelector(".prompt");
-let input = document.querySelector(".input-field")
-let submit = document.querySelector(".submit-button")
-let result = document.querySelector(".result")
+let inputA = document.querySelector(".input-a");
+let inputB = document.querySelector(".input-b");
+let inputC = document.querySelector(".input-c");
+let submit = document.querySelector(".submit-button");
+let result = document.querySelector(".result");
+
+submit.addEventListener("click", () => {
+    coA = inputA.value;
+    coB = inputB.value;
+    coC = inputC.value;
+    solveQuadratic(coA, coB, coC);
+});
+
+function solveQuadratic(A, B, C) {
+    let results = [];
+    results.push((B*-1 + (B**2 - 4*A*C)**(1/2))/2*A);
+    results.push((B*-1 - (B**2 - 4*A*C)**(1/2))/2*A);
+    console.log(results);
+}
