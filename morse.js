958a02457acbc39b3667d319fc6fbc3284704898
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-10-18 09:49:19 -0600

    created morse.js and morse.html

---
+++ morse.js
@@ -0,0 +1,12 @@
+morseCode = {'A':'.-', 'B': '-...', 'C':'-.-.', 'D':'-..', 'E':'.',
+    'F':'..-.', 'G':'--.', 'H':'....', 'I':'..', 'J':'.---',
+    'K':'-.-', 'L':'.-..', 'M':'--', 'N':'-.', 'O': '---', 'P':'.--.',
+    'Q':'--.-', 'R':'.-.', 'S':'...', 'T':'-', 'U':'..-',
+    'V':'...-', 'W':'.--', 'X':'-..-', 'Y':'-.--', 'Z':'--..'};
+
+console.log(morseCode);
+
+e2mInput = document.querySelector(".e2m");
+m2eInput = document.querySelector(".m2e");
+submit = document.querySelector(".submit");
+result = document.querySelector(".result");
