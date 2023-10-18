845fc3059b5781d2954fba5e6cc7f11c1d3d6ae0
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-10-18 17:12:55 -0600

    first working m2e translation

--- morse.js
+++ morse.js
@@ -13,8 +13,7 @@ submitMorse = document.querySelector(".submit-morse")
 result = document.querySelector(".result");
 
 submitEnglish.addEventListener("click", () => {
-    englishToTranslate = e2mInput.value.toUpperCase()
-    englishToTranslate = englishToTranslate.replace(/\s/g, '');
+    englishToTranslate = e2mInput.value.toUpperCase().replace(/\W/g, '');
     message = [];
     for (i = 0; i < englishToTranslate.length; i++) {
         char = englishToTranslate[i];
@@ -22,4 +21,21 @@ submitEnglish.addEventListener("click", () => {
     }
     message = message.join(' ');
     result.textContent = `Your message translated to morse is ${message}`;
+});
+
+submitMorse.addEventListener("click", () => {
+    morseToTranslate = m2eInput.value.split(' ');
+    console.log(morseToTranslate);
+    message = [];
+    for (i = 0; i < morseToTranslate.length; i++) {
+        char = morseToTranslate[i];
+        console.log(char);
+        for (key in morseCode) {
+            if (morseCode[key] == char) {
+                message.push(key);
+            }
+        }
+    }
+    message = message.join(' ');
+    result.textContent = `Your message translated to english is ${message}`;
 });
