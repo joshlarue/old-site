216bf3ae65d825309d88f28e102d2c9a826c1a53
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-11-04 12:29:23 -0600

    added event listener to submit btn and stored values in data dict

--- scripts/mortgagecalc.js
+++ scripts/mortgagecalc.js
@@ -2,3 +2,19 @@ const purchasePrice = document.querySelector(".purchase-price");
 const downPaymentPc = document.querySelector(".down-payment-percentage");
 const mortgageTerm = document.querySelector(".mortgage-term");
 const amortPeriod = document.querySelector(".amortization-period");
+const submitBtn = document.querySelector(".input-submit");
+const output = document.querySelector(".output");
+
+let data = {};
+
+submitBtn.addEventListener("click", () => {
+    if (!isNaN(purchasePrice.value) && !isNaN(downPaymentPc.value) && !isNaN(mortgageTerm.value) && !isNaN(amortPeriod.value)) {
+        data["purchasePrice"] = purchasePrice.value;
+        data["downPaymentPc"] = downPaymentPc.value;
+        data["mortgageTerm"] = mortgageTerm.value;
+        data["amortPeriod"] = amortPeriod.value;
+        output.textContent = "You entered all numbers!";
+    } else {
+        output.textContent = "Please enter all number values.";
+    }
+});
