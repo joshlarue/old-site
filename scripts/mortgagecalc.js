e033c4bc255566da4b4caf7c48bb157246129409
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-11-04 12:09:02 -0600

    skeleton structure for mortgage calculator

---
+++ scripts/mortgagecalc.js
@@ -0,0 +1,4 @@
+const purchasePrice = document.querySelector(".purchase-price");
+const downPaymentPc = document.querySelector(".down-payment-percentage");
+const mortgageTerm = document.querySelector(".mortgage-term");
+const amortPeriod = document.querySelector(".amortization-period");
