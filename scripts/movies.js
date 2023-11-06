016bf5ce59db0ae472fb04d8b2a506b2c76e49d8
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-11-06 13:25:19 -0700

    styling, js theming

--- scripts/movies.js
+++ scripts/movies.js
@@ -1,6 +1,8 @@
 const themeToggle = document.querySelector('.theme-toggle');
 const root = document.documentElement;
 
+window.onload = root.setAttribute('data-theme', 'light');
+
 themeToggle.addEventListener('click', function () {
   if (root.getAttribute('data-theme') === 'light') {
     root.setAttribute('data-theme', 'dark');
